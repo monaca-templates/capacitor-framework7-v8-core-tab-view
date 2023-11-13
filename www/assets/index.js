1f9dad53c7687f0fa10b1716c017ba95d612cc80
yong <yong@asial.co.jp>
2023-11-13 15:57:29 +0900

    feat: support css for Localkit

--- www/assets/index.js
+++ www/assets/index.js
@@ -299,7 +299,7 @@ ${l.toString()}`)}return n&&(typeof n=="string"?o+=`?${n}`:Object.keys(n).length
       </div>
     </div>
   </div>
-`}}Un.id="7384819841";function _n(){return function(s){s.$;var e=s.$h;return s.$root,s.$f7,s.$f7route,s.$f7router,s.$theme,s.$update,s.$store,e`
+`}}Un.id="5319489aab";function _n(){return function(s){s.$;var e=s.$h;return s.$root,s.$f7,s.$f7route,s.$f7router,s.$theme,s.$update,s.$store,e`
 <div class="page" data-name="about">
   <div class="navbar">
     <div class="navbar-bg"></div>
@@ -328,7 +328,7 @@ ${l.toString()}`)}return n&&(typeof n=="string"?o+=`?${n}`:Object.keys(n).length
     </div>
   </div>
 </div>
-`}}_n.id="45fc7bde3d";function Kn(){return function(s){s.$;var e=s.$h;return s.$root,s.$f7,s.$f7route,s.$f7router,s.$theme,s.$update,s.$store,e`
+`}}_n.id="6e5e7ec270";function Kn(){return function(s){s.$;var e=s.$h;return s.$root,s.$f7,s.$f7route,s.$f7router,s.$theme,s.$update,s.$store,e`
 <div class="page" data-name="form">
   <div class="navbar">
     <div class="navbar-bg"></div>
@@ -569,7 +569,7 @@ ${l.toString()}`)}return n&&(typeof n=="string"?o+=`?${n}`:Object.keys(n).length
     </div>
   </div>
 </div>
-`}}Kn.id="7282d45de2";function Qn(s,{$store:e}){const a=e.getters.products,t=()=>{e.dispatch("addProduct",{id:"4",title:"Apple iPhone 12",description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis."})};return function(n){n.$;var r=n.$h;return n.$root,n.$f7,n.$f7route,n.$f7router,n.$theme,n.$update,n.$store,r`
+`}}Kn.id="5035dca288";function Qn(s,{$store:e}){const a=e.getters.products,t=()=>{e.dispatch("addProduct",{id:"4",title:"Apple iPhone 12",description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis."})};return function(n){n.$;var r=n.$h;return n.$root,n.$f7,n.$f7route,n.$f7router,n.$theme,n.$update,n.$store,r`
   <div class="page" data-name="catalog">
     <div class="navbar">
       <div class="navbar-bg"></div>
@@ -598,7 +598,7 @@ ${l.toString()}`)}return n&&(typeof n=="string"?o+=`?${n}`:Object.keys(n).length
       `}
     </div>
   </div>
-`}}Qn.id="9177b5aa47";function Zn(s,{$f7route:e,$store:a}){const t=a.getters.products,n=e.params.id;let r;return t.value.forEach(i=>{i.id===n&&(r=i)}),function(i){i.$;var o=i.$h;return i.$root,i.$f7,i.$f7route,i.$f7router,i.$theme,i.$update,i.$store,o`
+`}}Qn.id="dfb0e68959";function Zn(s,{$f7route:e,$store:a}){const t=a.getters.products,n=e.params.id;let r;return t.value.forEach(i=>{i.id===n&&(r=i)}),function(i){i.$;var o=i.$h;return i.$root,i.$f7,i.$f7route,i.$f7router,i.$theme,i.$update,i.$store,o`
   <div class="page" data-name="product">
     <div class="navbar">
       <div class="navbar-bg"></div>
@@ -619,7 +619,7 @@ ${l.toString()}`)}return n&&(typeof n=="string"?o+=`?${n}`:Object.keys(n).length
       </div>
     </div>
   </div>
-`}}Zn.id="4d30df4c68";function Jn(){return function(s){s.$;var e=s.$h;return s.$root,s.$f7,s.$f7route,s.$f7router,s.$theme,s.$update,s.$store,e`
+`}}Zn.id="01d4abe932";function Jn(){return function(s){s.$;var e=s.$h;return s.$root,s.$f7,s.$f7route,s.$f7router,s.$theme,s.$update,s.$store,e`
 <div class="page" data-name="settings">
   <div class="navbar">
     <div class="navbar-bg"></div>
@@ -854,7 +854,7 @@ ${l.toString()}`)}return n&&(typeof n=="string"?o+=`?${n}`:Object.keys(n).length
     </div>
   </div>
 </div>
-`}}Jn.id="4a4a1621f5";function ts(s,{$f7:e,$on:a,$onBeforeMount:t,$onMounted:n,$onBeforeUnmount:r,$onUnmounted:i}){let o="Jimmy",l=25,c=["Tennis","Chess","Football"];const d=()=>{e.dialog.alert("Hello World")};return t(()=>{console.log("onBeforeMount")}),n(()=>{console.log("onMounted")}),r(()=>{console.log("onBeforeUnmount")}),i(()=>{console.log("onUnmounted")}),a("pageMounted",(u,p)=>{console.log("pageMounted",p)}),a("pageInit",(u,p)=>{console.log("pageInit",p)}),a("pageBeforeIn",(u,p)=>{console.log("pageBeforeIn",p)}),a("pageAfterIn",(u,p)=>{console.log("pageAfterIn",p)}),a("pageBeforeOut",(u,p)=>{console.log("pageBeforeOut",p)}),a("pageAfterOut",(u,p)=>{console.log("pageAfterOut",p)}),a("pageBeforeRemove",(u,p)=>{console.log("pageBeforeRemove",p)}),function(u){u.$;var p=u.$h;u.$root,u.$f7;var g=u.$f7route;u.$f7router;var h=u.$theme;return u.$update,u.$store,p`
+`}}Jn.id="115ecb0a80";function ts(s,{$f7:e,$on:a,$onBeforeMount:t,$onMounted:n,$onBeforeUnmount:r,$onUnmounted:i}){let o="Jimmy",l=25,c=["Tennis","Chess","Football"];const d=()=>{e.dialog.alert("Hello World")};return t(()=>{console.log("onBeforeMount")}),n(()=>{console.log("onMounted")}),r(()=>{console.log("onBeforeUnmount")}),i(()=>{console.log("onUnmounted")}),a("pageMounted",(u,p)=>{console.log("pageMounted",p)}),a("pageInit",(u,p)=>{console.log("pageInit",p)}),a("pageBeforeIn",(u,p)=>{console.log("pageBeforeIn",p)}),a("pageAfterIn",(u,p)=>{console.log("pageAfterIn",p)}),a("pageBeforeOut",(u,p)=>{console.log("pageBeforeOut",p)}),a("pageAfterOut",(u,p)=>{console.log("pageAfterOut",p)}),a("pageBeforeRemove",(u,p)=>{console.log("pageBeforeRemove",p)}),function(u){u.$;var p=u.$h;u.$root,u.$f7;var g=u.$f7route;u.$f7router;var h=u.$theme;return u.$update,u.$store,p`
   <div class="page">
     <div class="navbar">
       <div class="navbar-bg"></div>
@@ -910,7 +910,7 @@ ${l.toString()}`)}return n&&(typeof n=="string"?o+=`?${n}`:Object.keys(n).length
       </div>
     </div>
   </div>
-`}}ts.id="d8aba722aa";ts.style=`
+`}}ts.id="789cecf2bb";ts.style=`
   p {
     margin: 10px 0;
   }
@@ -941,7 +941,7 @@ ${l.toString()}`)}return n&&(typeof n=="string"?o+=`?${n}`:Object.keys(n).length
       </div>
     </div>
   </div>
-`}}er.id="034ca03887";function tr(){return function(s){s.$;var e=s.$h;return s.$root,s.$f7,s.$f7route,s.$f7router,s.$theme,s.$update,s.$store,e`
+`}}er.id="1d18953aa2";function tr(){return function(s){s.$;var e=s.$h;return s.$root,s.$f7,s.$f7route,s.$f7router,s.$theme,s.$update,s.$store,e`
 <div class="page">
   <div class="navbar">
     <div class="navbar-bg"></div>
@@ -962,7 +962,7 @@ ${l.toString()}`)}return n&&(typeof n=="string"?o+=`?${n}`:Object.keys(n).length
     </div>
   </div>
 </div>
-`}}tr.id="63a24972c7";var Kp=[{path:"/",component:Un},{path:"/about/",component:_n},{path:"/form/",component:Kn},{path:"/catalog/",component:Qn},{path:"/product/:id/",component:Zn},{path:"/settings/",component:Jn},{path:"/dynamic-route/blog/:blogId/post/:postId/",component:ts},{path:"/request-and-load/user/:userId/",async:function({router:s,to:e,resolve:a}){var t=s.app;t.preloader.show(),e.params.userId,setTimeout(function(){var n={firstName:"Vladimir",lastName:"Kharlampidi",about:"Hello, i am creator of Framework7! Hope you like it!",links:[{title:"Framework7 Website",url:"http://framework7.io"},{title:"Framework7 Forum",url:"http://forum.framework7.io"}]};t.preloader.hide(),a({component:er},{props:{user:n}})},1e3)}},{path:"(.*)",component:tr}];Bt.use([Cn]);Ua.use([fn,hn,mn,gn,vn,$n,Ka,In,An,On,Bn,Ln,Dn,Rn,Hn,zn,Fn,Vn]);const Qp=Ha({state:{products:[{id:"1",title:"Apple iPhone 8",description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis."},{id:"2",title:"Apple iPhone 8 Plus",description:"Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!"},{id:"3",title:"Apple iPhone X",description:"Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum."}]},getters:{products({state:s}){return s.products}},actions:{addProduct({state:s},e){s.products=[...s.products,e]}}});function ar(s,{$f7:e,$update:a}){let t="",n="";const r=l=>{t=l.target.value,a()},i=l=>{n=l.target.value,a()},o=()=>{e.dialog.alert("Username: "+t+"<br/>Password: "+n,()=>{e.loginScreen.close()})};return function(l){l.$;var c=l.$h;return l.$root,l.$f7,l.$f7route,l.$f7router,l.$theme,l.$update,l.$store,c`
+`}}tr.id="733494914d";var Kp=[{path:"/",component:Un},{path:"/about/",component:_n},{path:"/form/",component:Kn},{path:"/catalog/",component:Qn},{path:"/product/:id/",component:Zn},{path:"/settings/",component:Jn},{path:"/dynamic-route/blog/:blogId/post/:postId/",component:ts},{path:"/request-and-load/user/:userId/",async:function({router:s,to:e,resolve:a}){var t=s.app;t.preloader.show(),e.params.userId,setTimeout(function(){var n={firstName:"Vladimir",lastName:"Kharlampidi",about:"Hello, i am creator of Framework7! Hope you like it!",links:[{title:"Framework7 Website",url:"http://framework7.io"},{title:"Framework7 Forum",url:"http://forum.framework7.io"}]};t.preloader.hide(),a({component:er},{props:{user:n}})},1e3)}},{path:"(.*)",component:tr}];Bt.use([Cn]);Ua.use([fn,hn,mn,gn,vn,$n,Ka,In,An,On,Bn,Ln,Dn,Rn,Hn,zn,Fn,Vn]);const Qp=Ha({state:{products:[{id:"1",title:"Apple iPhone 8",description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis."},{id:"2",title:"Apple iPhone 8 Plus",description:"Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!"},{id:"3",title:"Apple iPhone X",description:"Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum."}]},getters:{products({state:s}){return s.products}},actions:{addProduct({state:s},e){s.products=[...s.products,e]}}});function ar(s,{$f7:e,$update:a}){let t="",n="";const r=l=>{t=l.target.value,a()},i=l=>{n=l.target.value,a()},o=()=>{e.dialog.alert("Username: "+t+"<br/>Password: "+n,()=>{e.loginScreen.close()})};return function(l){l.$;var c=l.$h;return l.$root,l.$f7,l.$f7route,l.$f7router,l.$theme,l.$update,l.$store,c`
   <div id="app">
 
     <!-- Left panel with cover effect-->
@@ -1103,4 +1103,4 @@ ${l.toString()}`)}return n&&(typeof n=="string"?o+=`?${n}`:Object.keys(n).length
     </div>
 
   </div>
-`}}ar.id="687c948008";var rn=ue();new Ua({name:"capacitor-framework7-v8-core-tab-view",theme:"auto",el:"#app",component:ar,store:Qp,routes:Kp,input:{scrollIntoViewOnFocus:rn.capacitor,scrollIntoViewCentered:rn.capacitor},statusbar:{iosOverlaysWebView:!0,androidOverlaysWebView:!1},on:{init:function(){var s=this;s.device.capacitor&&ot.init(s)}}});
+`}}ar.id="3c47be8914";var rn=ue();new Ua({name:"capacitor-framework7-v8-core-tab-view",theme:"auto",el:"#app",component:ar,store:Qp,routes:Kp,input:{scrollIntoViewOnFocus:rn.capacitor,scrollIntoViewCentered:rn.capacitor},statusbar:{iosOverlaysWebView:!0,androidOverlaysWebView:!1},on:{init:function(){var s=this;s.device.capacitor&&ot.init(s)}}});
